9af4ab004d0da1b1b8394db1d6c183c2cb8e2b86
James Ambrose <galaxy2198@gmail.com>
2020-08-09 23:44:41 +0000

    fixed some errors

--- startGrab.sh
+++ startGrab.sh
@@ -4,6 +4,6 @@ if [ -f .env ]
 then
   export $(cat .env | sed 's/#.*//g' | xargs)
   docker rm -f $DOCKER_IMAGE_NAME"_"$DOCKER_IMAGE_NAME"_1"
-  docker compose up -d
+  docker-compose up -d
   docker logs -f $DOCKER_IMAGE_NAME"_"$DOCKER_IMAGE_NAME"_1" >> output.log
 fi
